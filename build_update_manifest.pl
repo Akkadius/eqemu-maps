be73cabb0a6b1a1aff87fadf60b50207d8f0ff51
Akkadius <akkadius1@gmail.com>
2015-03-11 03:28:50 -0500

    Structure update

--- build_update_manifest.pl
+++ build_update_manifest.pl
@@ -6,7 +6,7 @@ use File::Find;
 open (MAPS_MANIFEST, '> !eqemu_maps_manifest.txt');
 
 my @files;
-my $start_dir = ".";
+my $start_dir = "maps/";
 find( 
     sub { push @files, $File::Find::name unless -d; }, 
     $start_dir
@@ -14,7 +14,6 @@ find(
 for my $file (@files) {
 	if($file=~/\.map|\.wtr|\.path/i){
 		my $file_size = (stat $file)[7];
-		$file =~s/\.\///g;
 		print $file . "," . $file_size . "\n";
 		print MAPS_MANIFEST $file . "," . $file_size . "\n";
 	}
