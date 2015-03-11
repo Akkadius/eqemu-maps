7bd31101ffedee3428208107c4f071daa170f434
Akkadius <akkadius1@gmail.com>
2015-03-11 01:56:17 -0500

    Update maps manifest and add build_update_manifest.pl

---
+++ build_update_manifest.pl
@@ -0,0 +1,23 @@
+#::: Akkadius
+#::: Build manifest text file for eqemu_update.pl
+
+use File::Find;
+
+open (MAPS_MANIFEST, '> !eqemu_maps_manifest.txt');
+
+my @files;
+my $start_dir = ".";
+find( 
+    sub { push @files, $File::Find::name unless -d; }, 
+    $start_dir
+);
+for my $file (@files) {
+	if($file=~/\.map|\.wtr|\.path/i){
+		my $file_size = (stat $file)[7];
+		$file =~s/\.\///g;
+		print $file . "," . $file_size . "\n";
+		print MAPS_MANIFEST $file . "," . $file_size . "\n";
+	}
+}
+
+close (MAPS_MANIFEST);
